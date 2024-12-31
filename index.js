95e6ace12f7443a9299bb26f7597ef6a20a51cc5
Shirooo098 <johndavid8803@gmail.com>
2024-12-31 21:00:35 +0800

    Added Express/EJS/CSS

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
+
+app.listen(3000, () => {
+    console.log(`Server is running on port ${port}.`)
+});
